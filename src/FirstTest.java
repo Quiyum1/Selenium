54f09ea23abef46804a89121eb524043c7365863
Quiyum1 <a.quiyum07@gmail.com>
2020-04-16 14:42:36 -0400

    First commit

---
+++ src/FirstTest.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("http://google.com");
+		driver.manage().window().maximize();
+		
+	}
+
+}
